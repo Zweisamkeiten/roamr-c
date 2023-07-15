cf9e7366c04d3ea2c97626cbfcb05f6a4f68ee7a
Einsam <snitxmhm@gmail.com>
2023-07-15 18:54:05 +0800

    :rocket: feat(roamr.c): Hide the cursor when repainting

--- src/roamr.c
+++ src/roamr.c
@@ -208,12 +208,14 @@ void editorDrawRows(abuf *ab) {
 void editorRefreshScreen() {
   abuf ab = ABUF_INIT;
 
+  abAppend(&ab, "\x1b[?25l]h", 6);
   abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
