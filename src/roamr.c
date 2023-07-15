fa50a271ea779aafa5985d478eeea7e97762e579
Einsam <snitxmhm@gmail.com>
2023-07-15 13:41:45 +0800

    :rocket: feat(roamr.c): read the keypresses & press q to quit

--- src/roamr.c
+++ src/roamr.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    ;
+
   return EXIT_SUCCESS;
 }
