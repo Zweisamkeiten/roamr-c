567c77213cd45e9f45c5b1725fc43436aa00c0f6
Einsam <snitxmhm@gmail.com>
2023-07-15 15:48:01 +0800

    :rocket: feat(roamr.c): Display keypresses

--- src/roamr.c
+++ src/roamr.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -27,8 +29,13 @@ int main(int argc, char *argv[]) {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
 
   return EXIT_SUCCESS;
 }
