b200a3fea399a0f95ad3e719afb51fa1121acfc1
Einsam <snitxmhm@gmail.com>
2023-07-15 13:42:15 +0800

    :rocket: feat(roamr.c): Turn off echoing

--- src/roamr.c
+++ src/roamr.c
@@ -1,7 +1,23 @@
 #include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+/**
+ * @brief Turn off echoing
+ */
+void enableRawMode() {
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(int argc, char *argv[]) {
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     ;
