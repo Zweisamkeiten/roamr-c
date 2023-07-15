Zweisamkeiten/gdbstub

:memo: init project

Makefile
@@ -1,0 +1,88 @@
+# tool macros
+CC ?= gcc# FILL: the compiler
+CCFLAGS := -O2 -Wall -Werror -lreadline # FILL: compile flags
+DBGFLAGS := -g
+CCOBJFLAGS := -O2 -Wall -Werror $(CCOBJFLAGS) -c
+
+# path macros
+BIN_PATH := bin
+OBJ_PATH := obj
+SRC_PATH := src
+DBG_PATH := debug
+
+WORK_DIR  = $(shell pwd)
+
+INC_PATH := $(WORK_DIR)/include $(INC_PATH)
+INCLUDES = $(addprefix -I, $(INC_PATH))
+CCOBJFLAGS := $(INCLUDES) $(CCOBJFLAGS)
+
+# compile macros
+TARGET_NAME := main
+ifeq ($(OS),Windows_NT)
+	TARGET_NAME := $(addsuffix .exe,$(TARGET_NAME))
+endif
+TARGET := $(BIN_PATH)/$(TARGET_NAME)
+TARGET_DEBUG := $(DBG_PATH)/$(TARGET_NAME)
+
+# src files & obj files
+SRC := $(foreach x, $(SRC_PATH), $(wildcard $(addprefix $(x)/*,.c*)))
+OBJ := $(addprefix $(OBJ_PATH)/, $(addsuffix .o, $(notdir $(basename $(SRC)))))
+OBJ_DEBUG := $(addprefix $(DBG_PATH)/, $(addsuffix .o, $(notdir $(basename $(SRC)))))
+
+# clean files list
+DISTCLEAN_LIST := $(OBJ) \
+                  $(OBJ_DEBUG)
+CLEAN_LIST := $(TARGET) \
+			  $(TARGET_DEBUG) \
+			  $(DISTCLEAN_LIST)
+
+# default rule
+default: makedir all
+
+# non-phony targets
+$(TARGET): $(OBJ)
+	$(CC) $(CCFLAGS) -o $@ $(OBJ)
+
+$(OBJ_PATH)/%.o: $(SRC_PATH)/%.c*
+	@echo + CC $<
+	$(CC) $(CCOBJFLAGS) -o $@ $<
+
+$(DBG_PATH)/%.o: $(SRC_PATH)/%.c*
+	@echo + CC $<
+	$(CC) $(CCOBJFLAGS) $(DBGFLAGS) -o $@ $<
+	$(CC) -E $(INCLUDES) $< | \
+		grep -ve '^#' | \
+		clang-format - > $(basename $@).i
+
+$(TARGET_DEBUG): $(OBJ_DEBUG)
+	@echo + LD $@
+	$(CC) $(CCFLAGS) $(DBGFLAGS) $(OBJ_DEBUG) -o $@
+
+ARGS :=
+MAIN_EXEC := $(TARGET)
+
+# phony rules
+.PHONY: makedir
+makedir:
+	@mkdir -p $(BIN_PATH) $(OBJ_PATH) $(DBG_PATH)
+
+.PHONY: all
+all: $(TARGET)
+
+.PHONY: run
+run: all
+	$(MAIN_EXEC)
+
+.PHONY: debug
+debug: $(TARGET_DEBUG)
+	gdb $(TARGET_DEBUG) --args $(TARGET_DEBUG) $(ARGS)
+
+.PHONY: clean
+clean:
+	@echo CLEAN $(CLEAN_LIST)
+	@rm -f $(CLEAN_LIST)
+
+.PHONY: distclean
+distclean:
+	@echo CLEAN $(CLEAN_LIST)
+	@rm -f $(DISTCLEAN_LIST)

include/utils.h
@@ -1,0 +1,35 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#define ANSI_FG_BLACK   "\33[1;30m"
+#define ANSI_FG_RED     "\33[1;31m"
+#define ANSI_FG_GREEN   "\33[1;32m"
+#define ANSI_FG_YELLOW  "\33[1;33m"
+#define ANSI_FG_BLUE    "\33[1;34m"
+#define ANSI_FG_MAGENTA "\33[1;35m"
+#define ANSI_FG_CYAN    "\33[1;36m"
+#define ANSI_FG_WHITE   "\33[1;37m"
+#define ANSI_BG_BLACK   "\33[1;40m"
+#define ANSI_BG_RED     "\33[1;41m"
+#define ANSI_BG_GREEN   "\33[1;42m"
+#define ANSI_BG_YELLOW  "\33[1;43m"
+#define ANSI_BG_BLUE    "\33[1;44m"
+#define ANSI_BG_MAGENTA "\33[1;35m"
+#define ANSI_BG_CYAN    "\33[1;46m"
+#define ANSI_BG_WHITE   "\33[1;47m"
+#define ANSI_NONE       "\33[0m"
+
+#define ANSI_FMT(str, fmt) fmt str ANSI_NONE
+
+#define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
+
+#define _Log(...) \
+  do { \
+    printf(__VA_ARGS__); \
+  } while (0)
+
+#define Log(format, ...) \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
+#endif

Zweisamkeiten/roamr-c

Makefile
@@ -1,0 +1,88 @@
+# tool macros
+CC ?= gcc# FILL: the compiler
+CCFLAGS := -O2 -Wall -Werror # FILL: compile flags
+DBGFLAGS := -g
+CCOBJFLAGS := -O2 -Wall -Werror $(CCOBJFLAGS) -c
+
+# path macros
+BIN_PATH := bin
+OBJ_PATH := obj
+SRC_PATH := src
+DBG_PATH := debug
+
+WORK_DIR  = $(shell pwd)
+
+INC_PATH := $(WORK_DIR)/include $(INC_PATH)
+INCLUDES = $(addprefix -I, $(INC_PATH))
+CCOBJFLAGS := $(INCLUDES) $(CCOBJFLAGS)
+
+# compile macros
+TARGET_NAME := roamr
+ifeq ($(OS),Windows_NT)
+	TARGET_NAME := $(addsuffix .exe,$(TARGET_NAME))
+endif
+TARGET := $(BIN_PATH)/$(TARGET_NAME)
+TARGET_DEBUG := $(DBG_PATH)/$(TARGET_NAME)
+
+# src files & obj files
+SRC := $(foreach x, $(SRC_PATH), $(wildcard $(addprefix $(x)/*,.c*)))
+OBJ := $(addprefix $(OBJ_PATH)/, $(addsuffix .o, $(notdir $(basename $(SRC)))))
+OBJ_DEBUG := $(addprefix $(DBG_PATH)/, $(addsuffix .o, $(notdir $(basename $(SRC)))))
+
+# clean files list
+DISTCLEAN_LIST := $(OBJ) \
+                  $(OBJ_DEBUG)
+CLEAN_LIST := $(TARGET) \
+			  $(TARGET_DEBUG) \
+			  $(DISTCLEAN_LIST)
+
+# default rule
+default: makedir all
+
+# non-phony targets
+$(TARGET): $(OBJ)
+	$(CC) $(CCFLAGS) -o $@ $(OBJ)
+
+$(OBJ_PATH)/%.o: $(SRC_PATH)/%.c*
+	@echo + CC $<
+	$(CC) $(CCOBJFLAGS) -o $@ $<
+
+$(DBG_PATH)/%.o: $(SRC_PATH)/%.c*
+	@echo + CC $<
+	$(CC) $(CCOBJFLAGS) $(DBGFLAGS) -o $@ $<
+	$(CC) -E $(INCLUDES) $< | \
+		grep -ve '^#' | \
+		clang-format - > $(basename $@).i
+
+$(TARGET_DEBUG): $(OBJ_DEBUG)
+	@echo + LD $@
+	$(CC) $(CCFLAGS) $(DBGFLAGS) $(OBJ_DEBUG) -o $@
+
+ARGS :=
+MAIN_EXEC := $(TARGET)
+
+# phony rules
+.PHONY: makedir
+makedir:
+	@mkdir -p $(BIN_PATH) $(OBJ_PATH) $(DBG_PATH)
+
+.PHONY: all
+all: $(TARGET)
+
+.PHONY: run
+run: all
+	$(MAIN_EXEC)
+
+.PHONY: debug
+debug: $(TARGET_DEBUG)
+	gdb $(TARGET_DEBUG) --args $(TARGET_DEBUG) $(ARGS)
+
+.PHONY: clean
+clean:
+	@echo CLEAN $(CLEAN_LIST)
+	@rm -f $(CLEAN_LIST)
+
+.PHONY: distclean
+distclean:
+	@echo CLEAN $(CLEAN_LIST)
+	@rm -f $(DISTCLEAN_LIST)

include/utils.h
@@ -1,0 +1,35 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#define ANSI_FG_BLACK   "\33[1;30m"
+#define ANSI_FG_RED     "\33[1;31m"
+#define ANSI_FG_GREEN   "\33[1;32m"
+#define ANSI_FG_YELLOW  "\33[1;33m"
+#define ANSI_FG_BLUE    "\33[1;34m"
+#define ANSI_FG_MAGENTA "\33[1;35m"
+#define ANSI_FG_CYAN    "\33[1;36m"
+#define ANSI_FG_WHITE   "\33[1;37m"
+#define ANSI_BG_BLACK   "\33[1;40m"
+#define ANSI_BG_RED     "\33[1;41m"
+#define ANSI_BG_GREEN   "\33[1;42m"
+#define ANSI_BG_YELLOW  "\33[1;43m"
+#define ANSI_BG_BLUE    "\33[1;44m"
+#define ANSI_BG_MAGENTA "\33[1;35m"
+#define ANSI_BG_CYAN    "\33[1;46m"
+#define ANSI_BG_WHITE   "\33[1;47m"
+#define ANSI_NONE       "\33[0m"
+
+#define ANSI_FMT(str, fmt) fmt str ASNI_NONE
+
+#define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
+
+#define _Log(...) \
+  do { \
+    printf(__VA_ARGS__); \
+  } while (0)
+
+#define Log(format, ...) \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
+#endif