611da07a8d33b65fc1bfcbe2bd3c8c554da7f111
Einsam <snitxmhm@gmail.com>
2023-07-15 15:58:30 +0800

    :bug: fix(roamr.c): Fix C-m
    It turns out that the terminal is helpfully translating
    
    any carriage returns (13, '\r') inputted by the user into newlines (10, '\n').

--- src/roamr.c
+++ src/roamr.c
@@ -20,7 +20,7 @@ void enableRawMode() {
 
   struct termios raw = orig_termios;
 
-  raw.c_iflag &= ~(IXON);
+  raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
