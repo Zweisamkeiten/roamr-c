4d951ae4a3abbd4d8fa255eb7c3e26bc1c1e1a7a
Einsam <snitxmhm@gmail.com>
2023-07-15 18:29:39 +0800

    :rocket: feat(roamr.c): window size, the hard way

--- src/roamr.c
+++ src/roamr.c
@@ -85,6 +85,47 @@ char editorReadKey() {
 }
 
 /**
+ * @brief get the sursor position. `n` command can be used to query the terminal
+ * for status information. argument of 6 to ask for the cursor position.
+ *
+ * @param rows row of the cursor
+ * @param cols column of the cursor
+ * @return success or not.
+ */
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+    return -1;
+  }
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+      break;
+    }
+
+    if (buf[i] == 'R') {
+      break;
+    }
+
+    i++;
+  }
+
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') {
+    return -1;
+  }
+
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+    return -1;
+  }
+
+  return 0;
+}
+
+/**
  * @brief get terminal WindowSize
  *
  * @param rows editor E struct
@@ -95,7 +136,11 @@ int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+      return -1;
+    }
+
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
