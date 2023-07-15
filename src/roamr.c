92104f85d44f004b3c02ad7104d1d3aa5286275c
Einsam <snitxmhm@gmail.com>
2023-07-15 22:27:22 +0800

    :rocket: feat(roamr.c): Moving left/right at the begin/end of a line

--- src/roamr.c
+++ src/roamr.c
@@ -436,6 +436,9 @@ void editorMoveCursor(int key) {
   case ARROW_LEFT: {
     if (E.cx != 0) {
       E.cx--;
+    } else if (E.cy > 0) {
+      E.cy--;
+      E.cx = E.row[E.cy].size;
     }
     break;
   }
@@ -454,6 +457,9 @@ void editorMoveCursor(int key) {
   case ARROW_RIGHT: {
     if (row && E.cx < row->size) {
       E.cx++;
+    } else if (row && E.cx == row->size) {
+      E.cy++;
+      E.cx = 0;
     }
     break;
   }
