48ee528415b7599c16de8738914c85b5be2aab2b
Einsam <snitxmhm@gmail.com>
2023-07-16 16:32:25 +0800

    :rocket: feat(roamr.c): The Enter key

--- src/roamr.c
+++ src/roamr.c
@@ -308,15 +308,18 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 /**
- * @brief Append a new row
+ * @brief Insert a new row
  *
  * @param s string
  * @param len string length
  */
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows)
+    return;
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -409,10 +412,24 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 /**
@@ -486,7 +503,7 @@ void editorOpen(char *filename) {
         linelen--;
       }
 
-      editorAppendRow(line, linelen);
+      editorInsertRow(E.numrows, line, linelen);
     }
   }
   free(line);
@@ -777,7 +794,7 @@ void editorProcessKeypress() {
 
   switch (c) {
   case '\r':
-    /* TODO: return */
+    editorInsertNewline();
     break;
 
   case CTRL_KEY('q'): {
